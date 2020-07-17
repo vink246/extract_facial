90d187df74af8d509bc2c8d06e00c7192034ae37
Vineet Kulkarni <54698974+vink246@users.noreply.github.com>
2020-07-17 12:06:32 +0530

    Update __init__.py
    functions made directly exposed form sub-modules

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from extract_face.getEyeOpenClose import getEyeOpenClose
+from extract_face.getEyeOpenCloseIris import getEyeOpenCloseIris
+from extract_face.getMouthOpenClose import getMouthOpenClose
+from extract_face.extractRoi import extractRoi
