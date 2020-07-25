0551ceff1f60b14701f9b34a53bd704065fabb76
Vineet Kulkarni <54698974+vink246@users.noreply.github.com>
2020-07-25 20:38:06 +0530

    changed version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="extract_facial",
-    version="v2.1-alpha",
+    version="v3.1-alpha",
     author="Vineet Kulkarni",
     author_email="2vineetk@gmail.com",
     description="Extract facial features in Python such as the eyes and mouth from MTCNN's predictions and find whether eyes or mouths are closed/open.",
     packages=["extract_facial"],
     include_package_data=True,
     url = "https://github.com/vink246/extract_facial.git",
-    download_url = "https://github.com/vink246/extract_facial/archive/v2.1-alpha.tar.gz",
+    download_url = "https://github.com/vink246/extract_facial/archive/v3.1-alpha.tar.gz",
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
